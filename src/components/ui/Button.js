d2622e7c71c417d54616e8ff02105eb646265cca
maria123-cyber <marianoor.0557@gmail.com>
2025-10-19 18:33:40 +0500

    commit

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -5,6 +5,7 @@ import { Pressable, Text } from 'react-native';
  * Minimal UI Button (no styling).
  * Students will style this via 'styles/styles.js'.
  */
+
 export default function Button({ onPress, children, style, ...rest }) {
   return (
     <Pressable onPress={onPress} style={style} {...rest}>
